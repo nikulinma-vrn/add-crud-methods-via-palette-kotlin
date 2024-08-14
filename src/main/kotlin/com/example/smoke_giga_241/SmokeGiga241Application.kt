8a1b29ec49c9bd5bb8e56bf4b74dad829d83d642
m.nikulin <m.nikulin@haulmont.com>
2024-08-14 13:31:09 +0300

    INIT

---
+++ src/main/kotlin/com/example/smoke_giga_241/SmokeGiga241Application.kt
@@ -0,0 +1,11 @@
+package com.example.smoke_giga_241
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SmokeGiga241Application
+
+fun main(args: Array<String>) {
+	runApplication<SmokeGiga241Application>(*args)
+}
---
+++ src/test/kotlin/com/example/smoke_giga_241/SmokeGiga241ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.smoke_giga_241
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SmokeGiga241ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
