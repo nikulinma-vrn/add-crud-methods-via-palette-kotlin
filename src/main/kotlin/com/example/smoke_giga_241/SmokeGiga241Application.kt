6d413bf34955f7db233246f6a83ec22cc0105442
m.nikulin <m.nikulin@haulmont.com>
2024-08-16 12:46:20 +0300

    Generator created

--- src/main/kotlin/com/example/smoke_giga_241/SmokeGiga241Application.kt
+++ src/main/kotlin/com/example/smoke_giga_241/SmokeGiga241Application.kt
@@ -1,6 +1,5 @@
 package com.example.smoke_giga_241
 
-import com.example.smoke_giga_241.utils.UserGenerator
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -10,5 +9,4 @@ class SmokeGiga241Application
 fun main(args: Array<String>) {
 	runApplication<SmokeGiga241Application>(*args)
 
-	UserGenerator.bulkUserAdd(50)
 }
--- src/main/kotlin/com/example/smoke_giga_241/service/UserServiceImpl.kt
+++ src/main/kotlin/com/example/smoke_giga_241/service/UserServiceImpl.kt
@@ -1,8 +1,32 @@
 package com.example.smoke_giga_241.service;
 
+import com.example.smoke_giga_241.entity.User
 import com.example.smoke_giga_241.repository.UserRepository
+import jakarta.annotation.PostConstruct
+import net.andreinc.mockneat.MockNeat
+import net.andreinc.mockneat.types.enums.NameType
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class UserServiceImpl(private val userRepository: UserRepository) : UserService {
+    
+    @EventListener(ApplicationReadyEvent::class)
+    fun bulkUserCreate() {
+        val mockNeat = MockNeat.threadLocal()
+
+        repeat(100) {
+            val user = User(
+                UUID.randomUUID(),
+                mockNeat.names().type(NameType.FIRST_NAME).get(),
+                mockNeat.users().get(),
+                mockNeat.emails().get(),
+                mockNeat.genders().get(),
+                mockNeat.creditCards().get()
+            )
+            userRepository.save(user)
+        }
+    }
 }
