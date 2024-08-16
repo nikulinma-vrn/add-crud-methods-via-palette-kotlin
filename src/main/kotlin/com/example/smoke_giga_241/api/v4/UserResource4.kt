7453b539f784f685759f1816afdd0ba5ab7b5c23
m.nikulin <m.nikulin@haulmont.com>
2024-08-16 16:34:15 +0300

    Api v1, v2, v3, v4 completed. All test passed.

--- src/main/kotlin/com/example/smoke_giga_241/api/v4/UserResource4.kt
+++ src/main/kotlin/com/example/smoke_giga_241/api/v4/UserResource4.kt
@@ -1,7 +1,11 @@
 package com.example.smoke_giga_241.api.v4
 
-import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.bind.annotation.RequestMapping
+import com.example.smoke_giga_241.dto.UserDto
+import com.example.smoke_giga_241.service.UserService
+import com.fasterxml.jackson.databind.JsonNode
+import org.springframework.web.bind.annotation.*
+import java.io.IOException
+import java.util.*
 
 /*
  * ToDo: Amplicode Designer -> Request Handling -> Get All
@@ -55,9 +59,58 @@ import org.springframework.web.bind.annotation.RequestMapping
 
 @RestController
 @RequestMapping("/rest/v4")
-class UserResource4 {
+class UserResource4(private val userService: UserService) {
+    @GetMapping
+    fun getList(@ModelAttribute filter: UserFilter?): List<UserDto> {
+        return userService.getList(filter)
+    }
 
-    
 
+    @GetMapping("/{id}")
+    fun getOne(@PathVariable id: UUID): UserDto {
+        return userService.getOne(id)
+    }
+
+    @GetMapping("/by-ids")
+    fun getMany(@RequestParam ids: List<UUID>): List<UserDto> {
+        return userService.getMany(ids)
+    }
+
+    @PostMapping
+    fun create(@RequestBody dto: UserDto): UserDto {
+        return userService.create(dto)
+    }
+
+    @PostMapping("/bulk")
+    fun createMany(@RequestBody dtos: List<UserDto>): List<UserDto> {
+        return userService.createMany(dtos)
+    }
+
+    @PatchMapping("/{id}")
+    @Throws(IOException::class)
+    fun patch(@PathVariable id: UUID, @RequestBody patchNode: JsonNode): UserDto {
+        return userService.patch(id, patchNode)
+    }
+
+    @PatchMapping
+    @Throws(IOException::class)
+    fun patchMany(@RequestParam ids: List<UUID>, @RequestBody patchNode: JsonNode): List<UUID> {
+        return userService.patchMany(ids, patchNode)
+    }
+
+    @PutMapping("/{id}")
+    fun update(@PathVariable id: UUID, @RequestBody dto: UserDto): UserDto {
+        return userService.update(id, dto)
+    }
+
+    @DeleteMapping("/{id}")
+    fun delete(@PathVariable id: UUID): UserDto? {
+        return userService.delete(id)
+    }
+
+    @DeleteMapping
+    fun deleteMany(@RequestParam ids: List<UUID>) {
+        userService.deleteMany(ids)
+    }
 }
 
